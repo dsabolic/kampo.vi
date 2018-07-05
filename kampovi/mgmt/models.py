83897eecfd4b1c4ad9729d2d33c18167889543b0
Domagoj Sabolic <domagojsabolic2003@gmail.com>
2018-07-05 13:50:29 +0200

    Model Kamp

--- kampovi/mgmt/models.py
+++ kampovi/mgmt/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Kamp(models.Model):
+	name = models.CharField(
+		max_length=150
+	)
+
+	def __str__(self):
+		return self.name
+
+	start_date = models.DateTimeField(
+		'Pocetak kampa'
+	)
+	end_date = models.DateTimeField(
+		'Kraj kampa'
+	)
