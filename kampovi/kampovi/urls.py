bb98fd9a0436c19780869d5371b8e1df072e8cad
Eugen Bosnjak <eugen3141@gmail.com>
2018-07-06 13:57:56 +0200

    started creating user login interface

--- kampovi/kampovi/urls.py
+++ kampovi/kampovi/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
 	path('mgmt/', include('mgmt.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
